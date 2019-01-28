640de43804364747268a2b87dc20361040024c15
Maksym Kovalko <maxim.kovalko@macys.com>
2019-01-28 23:14:25 +0200

    Initial Commit

---
+++ WebBrowserExecuteJavaScript/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebBrowserExecuteJavaScript
+//
+//  Created by Maxim Kovalko on 1/28/19.
+//  Copyright © 2019 Maxim Kovalko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
